7c0da7fcc8e5687b49e2b70f92a2786ed873108a
葱花滑蛋 <conghuahuadan@163.com>
2018-12-27 17:18:36 +0800

    ...

---
+++ src/main/java/com/chhd/y/dto/VisitChartDTO.java
@@ -0,0 +1,23 @@
+package com.chhd.y.dto;
+
+public class VisitChartDTO {
+
+    private String x;
+    private int y;
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
